a299aeb26f07d3ed43b11cc6496ffe332dfaf39c
agblacky <70974889+agblacky@users.noreply.github.com>
2023-05-11 11:38:59 +0200

    Improved code quality and security

--- png2pdf.py
+++ png2pdf.py
@@ -1,17 +1,25 @@
 import glob
 from PIL import Image
 
-images = [
-    Image.open("./" + f)
-    for f in glob.glob("*.png")
-]
-index=0
-while index < len(images):
-    images[index]=images[index].convert("RGB")
-    index+=1
+images = []
+file_extensions=["png","jpg","jpeg"]
+for file_ext in file_extensions:
+    for f in glob.glob(f"*.{file_ext}"):
+        try:
+            image = Image.open(f).convert("RGB")
+            images.append(image)
+        except Exception as err:
+            print(f"Unable to open file {f} : {err}")
 
-pdf_path = "./test.pdf"
-    
-images[0].save(
-    pdf_path, "PDF" ,resolution=100.0, save_all=True#, append_images=images[1:]
-)
+pdf_path = "./output.pdf"
+try:
+    images[0].save(
+        pdf_path, "PDF", resolution=100.0, save_all=True, append_images=images[1:]
+    )
+except IndexError:
+    print("No images found.")
+except Exception as e:
+    print(f"An error occurred while saving the PDF file: {e}")
+finally:
+    for image in images:
+        image.close()
